6bd8cf26f85dd81466242517ad86d9127fc62684
Luisa Pichardo <luisapichardo@gmail.com>
2018-08-02 14:02:16 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Sup Dude. Your smooth like Lance but hot like Keith ; smart like Pidge and sweet like hunk ; brave like Shiro but curious like Allura and funny like Coran"
   end
 
 end
